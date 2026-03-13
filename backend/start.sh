ccf1e71a9064d8384e71f96759c0a146e29bf070
Priscillia <priscilliachryso@gmail.com>
2026-03-13 20:40:23 +0300

    Fix PHP Sybtax Error

--- backend/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# S'assurer que les répertoires de cache existent avec les bonnes permissions
-mkdir -p var/cache/prod var/logs
-chown -R www-data:www-data var
-chmod -R 775 var
-
-# Démarrer PHP-FPM
-php-fpm
