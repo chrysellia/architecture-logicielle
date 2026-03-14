e25ad86f87226f2512afa1667ce5b6518f565b3a
Priscillia <priscilliachryso@gmail.com>
2026-03-14 22:50:22 +0300

    Updated start.sh et dockerfile backend

--- backend/start.sh
+++ backend/start.sh
@@ -1,19 +1,5 @@
 #!/bin/sh
-
-export APP_ENV=prod
-export APP_DEBUG=0
-
 mkdir -p /var/run
-mkdir -p /var/www/html/var/cache/prod
-chown -R www-data:www-data /var/www/html/var
-
-# Vider et reconstruire le cache en tant que www-data
-su-exec www-data php /var/www/html/bin/console cache:clear --env=prod
-su-exec www-data php /var/www/html/bin/console cache:warmup --env=prod
-
-# Démarrer PHP-FPM en arrière-plan
 php-fpm &
-
 sleep 2
-
 exec nginx -g "daemon off;"
