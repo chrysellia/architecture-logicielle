f7aaecbe2774fe4c561ae111b4f175c9ef27abed
Priscillia <priscilliachryso@gmail.com>
2026-03-14 22:39:58 +0300

    updated dockerfile

--- backend/start.sh
+++ backend/start.sh
@@ -4,10 +4,12 @@ export APP_ENV=prod
 export APP_DEBUG=0
 
 mkdir -p /var/run
+mkdir -p /var/www/html/var/cache/prod
+chown -R www-data:www-data /var/www/html/var
 
-# Vider et reconstruire le cache
-php /var/www/html/bin/console cache:clear --env=prod --no-warmup
-php /var/www/html/bin/console cache:warmup --env=prod
+# Vider et reconstruire le cache en tant que www-data
+su-exec www-data php /var/www/html/bin/console cache:clear --env=prod
+su-exec www-data php /var/www/html/bin/console cache:warmup --env=prod
 
 # Démarrer PHP-FPM en arrière-plan
 php-fpm &
